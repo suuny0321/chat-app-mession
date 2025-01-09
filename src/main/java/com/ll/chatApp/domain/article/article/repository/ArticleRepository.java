b7a77b2f10f4683d25b81d2002287293a5dc89d3
suuny0321 <joosun7647@naver.com>
2025-01-09 15:18:46 +0900

    member join

--- src/main/java/com/ll/chatApp/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/chatApp/domain/article/article/repository/ArticleRepository.java
@@ -1,10 +1,9 @@
 package com.ll.chatApp.domain.article.article.repository;
 
 
-import com.ll.chatApp.domain.article.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArticleRepository extends JpaRepository<Article,String> {
+public interface Article extends JpaRepository<Article,Long> {
 }
