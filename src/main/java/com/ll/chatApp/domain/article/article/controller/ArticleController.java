7d3dbbbc65d4bab2491963625ec302265b0756c4
suuny0321 <joosun7647@naver.com>
2025-01-09 17:48:42 +0900

    1

--- src/main/java/com/ll/chatApp/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/chatApp/domain/article/article/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.ll.chatApp.domain.article.article.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import com.ll.chatApp.domain.article.article.entity.Article;
 import com.ll.chatApp.domain.article.article.servise.ArticleService;
@@ -16,22 +17,45 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 
 @Controller
 @RequestMapping("/article")
 @RequiredArgsConstructor
+@Slf4j
 public class ArticleController {
     private final ArticleService articleService;
+
     @GetMapping("/list")
     public String list(
-            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "kwType", defaultValue = "") List<String> kwTypes,
+            @RequestParam(defaultValue = "") String kw,
+            @RequestParam(defaultValue = "0") int page,
            Model model
     ) {
+
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("id"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+
+        Map<String, Boolean> kwTypesMap = kwTypes
+                .stream()
+                .collect(Collectors.toMap(
+                        kwType -> kwType,
+                        kwType -> true
+                ));
+        log.debug("kwTypesMap: {}", kwTypesMap);
         Page<Article> itemsPage = articleService.search(pageable);
         model.addAttribute("itemsPage", itemsPage);
+        model.addAttribute("kwTypesMap", kwTypesMap);
+
         return "article/list";
+    }
+
+    @GetMapping("/a")
+    public String test(){
+        return "article/list2";
     }
 }
